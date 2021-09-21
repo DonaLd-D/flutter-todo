4803e55627cbd611660591a5cf5479cf1d337d9d
DonaLd-D <1376825153@qq.com>
2021-09-21 13:55:44 +0800

    :art: update

---
+++ lib/todo_list.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:todo_list/todo.dart';
+
+class TodoList extends StatefulWidget {
+  @override
+  _TodoListState createState() => _TodoListState();
+}
+
+class _TodoListState extends State<TodoList> {
+  List<Todo> todos = [
+    Todo(title: 'Learn Dart'),
+    Todo(title: 'Try Flutter'),
+    Todo(title: 'Be amazed'),
+  ];
+  _toggleTodo(Todo todo, bool isChecked) {
+    print('${todo.title} ${todo.isDone}');
+    setState(() {
+      todo.isDone = isChecked;
+    });
+  }
+
+  Widget _buildItem(BuildContext context, int index) {
+    final todo = todos[index];
+    return CheckboxListTile(
+      value: todo.isDone,
+      title: Text(todo.title),
+      onChanged: (bool isChecked) {
+        _toggleTodo(todo, isChecked);
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: _buildItem,
+      itemCount: todos.length,
+    );
+  }
+}
